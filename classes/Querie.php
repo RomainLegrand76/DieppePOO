1e9f1b7016a0bb4f6262dca2695ca5b0f01187c7
RomainLegrand76 <legrand.romain76@sfr.fr>
2018-04-27 09:39:06 +0200

    Ajout des fonction constructeur et destructeur

---
+++ classes/Querie.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dell
+ * Date: 26/04/2018
+ * Time: 16:55
+ */
+
+namespace tools;
+
+
+class Querie
+{
+    private $dsn = "mysql:dbname=easyWedding; host=localhost; charset=utf8";
+    private $user = "root";
+    private $password = "";
+    private $db;
+
+
+    public function __construct()
+    {
+        try
+        {
+            $this->db = new PDO($this->dsn, $this->user, $this->password);
+            $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+        }
+
+        catch (PDOExecption $e)
+        {
+            Log::logWrite($e->getMessage());
+        }
+
+    }
+
+    public function selectMethod($sql)
+    {
+
+    }
+
+    public function __destruct()
+    {
+        unset($this->db);
+    }
+}
